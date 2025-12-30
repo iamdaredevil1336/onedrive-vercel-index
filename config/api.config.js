5cd6a3953f5453cd769cab9648f5e2ed35b120ca
iamdaredevil1336 <siddha1336h@gmail.com>
2025-12-30 14:59:46 +0530

    Remove obfuscatedClientSecret from api.config.js
    Removed the obfuscated client secret for security reasons.

--- config/api.config.js
+++ config/api.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
   clientId: '374a23d0-d4ef-40e3-9572-6e43f9c575bb',
-  obfuscatedClientSecret: 'U2FsdGVkX19XXL7EMXITpNUx/lgKSIt44xW3RVxdlchPzYFKq7FUrPvlhiMQLuLFORqkOZytpRKT0wj4mF+MbA==',
+  obfuscatedClientSecret: '',
 
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
