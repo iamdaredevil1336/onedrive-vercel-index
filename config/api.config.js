eed863bbcdf3ed4079159b8fcd5053ffbc49585b
iamdaredevil1336 <siddha1336h@gmail.com>
2025-12-30 13:28:04 +0530

    Change API credentials in api.config.js
    Updated clientId and obfuscatedClientSecret for Microsoft Graph API authentication.

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: '83d7e13e-b3ed-42bb-af15-f69579acd94e',
-  obfuscatedClientSecret: 'U2FsdGVkX1+U6yx1wwdyTVvkVzyCYIkDS5IfBbvAnW+zdVYqG1mEswt6X6yMbt/sF+7JNTeCVGue9wS/fzOYnA==',
+  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
+  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
 
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
